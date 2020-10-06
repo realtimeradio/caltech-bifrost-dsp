2de2293b13d16df012270f20607e8a6036aac591
JackH <jack@rtr>
2020-10-06 19:05:51 +0000

    Add partial vis matrix output control

---
+++ pipeline/cm/lwa352_x_control.py
@@ -0,0 +1,36 @@
+import argparse
+import time
+import sys
+import logging
+
+from etcd_control import EtcdCorrControl
+
+from blocks.corr_output_full_control import CorrOutputFull
+from blocks.corr_output_part_control import CorrOutputPart
+from blocks.corr_acc_control import CorrAcc
+from blocks.corr_control import Corr
+
+default_log = logging.getLogger(__name__)
+logFormat = logging.Formatter('%(asctime)s [%(levelname)-8s] %(message)s',
+                              datefmt='%Y-%m-%d %H:%M:%S')
+logFormat.converter = time.gmtime
+logHandler = logging.StreamHandler(sys.stdout)
+logHandler.setFormatter(logFormat)
+logHandler.setLevel(logging.DEBUG)
+default_log.addHandler(logHandler)
+default_log.setLevel(logging.DEBUG)
+
+class Lwa352XControl():
+    def __init__(self, host='rtr-dev2', pipeline_id=0, etcdhost='etcdhost', log=default_log):
+        self.host = host
+        self.pipeline_id = pipeline_id
+        self.log = log
+        self.corr_interface = EtcdCorrControl(etcdhost=etcdhost, keyroot_cmd='/cmd/corr/x', keyroot_mon='/mon/corr/x', log=log)
+
+        self.corr_output_full = CorrOutputFull(self.log, self.corr_interface, self.host, self.pipeline_id)
+        self.corr_output_part = CorrOutputPart(self.log, self.corr_interface, self.host, self.pipeline_id)
+        self.corr = Corr(self.log, self.corr_interface, self.host, self.pipeline_id)
+        self.corr_acc = CorrAcc(self.log, self.corr_interface, self.host, self.pipeline_id)
+        
+
+
