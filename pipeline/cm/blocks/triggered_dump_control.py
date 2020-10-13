f30c3e929889e07572d885718a6f5de351703cca
JackH <jack@rtr>
2020-10-13 16:51:41 +0000

    Add control for triggered dump block

---
+++ pipeline/cm/blocks/triggered_dump_control.py
@@ -0,0 +1,10 @@
+from blocks.block_control_base import BlockControl
+import numpy as np
+
+class TriggeredDump(BlockControl):
+    def trigger(self, ntime_per_file=None, nfile=None, dump_path=None):
+       self.send_command(command='trigger', nfile=nfile, ntime_per_file=ntime_per_file, dump_path=dump_path)
+    def abort(self):
+       self.send_command(command='abort')
+    def stop(self):
+       self.send_command(command='stop')
