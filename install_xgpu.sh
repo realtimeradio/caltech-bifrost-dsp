9a5b44b587dd08fcbcb14c3ec6f94ec7d64e6c4d
JackH <jack@rtr>
2021-02-11 12:46:08 +0000

    Add xgpu install command

---
+++ install_xgpu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CURDIR=`pwd`
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $SCRIPTDIR/xgpu/src; make clean && make NSTATION=352 NFREQUENCY=184 NTIME=480 NTIME_PIPE=480 CUDA_ARCH=sm_75 DP4A=yes DEVSWIZZLE=yes && sudo make install
+cd $CURDIR
