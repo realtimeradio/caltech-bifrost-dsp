3c86079aa473d91919ee9d30d84d16610d8273b0
jack@RTR <jack@realtimeradio.co.uk>
2021-03-19 07:39:15 -0700

    Add template romein block

---
+++ pipeline/lwa352_pipeline/blocks/romein.py
@@ -0,0 +1,107 @@
+from .block_base import Block
+from bifrost.proclog import ProcLog
+import bifrost.ndarray as BFArray
+from bifrost.ndarray import copy_array
+from bifrost.device import stream_synchronize, set_device as BFSetGPU
+import bifrost.affinity as cpu_affinity
+import numpy as np
+import ujson as json
+import time
+
+from bifrost.romein import Romein
+
+class RomeinNoFFT(Block):
+    """ Transform block to grid and image pipeline and collate data
+    TODO: How to check?
+    Args:
+        iring: Ring to read from
+        conv: convolutional grid size
+        grid: total grid size
+    """
+    def __init__(self, log, iring, oring, conv, grid, core, nant, gpu=-1):
+        super().__init__(log, iring, oring, True, core, etcd_client=None)
+        self.iring = iring
+        self.oring = oring
+        self.grid_size = grid
+        self.conv_grid = conv
+        self.nant = nant
+        self.npol = 2
+        self.igulp_size = (nant*(nant-1)) // 2 * self.npol * self.npol * 2 * 4 # one channel of vis data
+        self.ogulp_size = self.npol * self.grid_size * self.grid_size * 2 * 8 #complex64
+
+        self.gpu = gpu
+
+    def sendToThreadListener(self, msg):
+        self.log.info(msg)
+
+    def main(self):
+        cpu_affinity.set_core(self.core)
+        if self.gpu != -1:
+            BFSetGPU(self.gpu)
+        self.sendToThreadListener("starting")
+
+        # w-kernel convolutions: just 1s
+        # convolution kernel shape: (channels, polarisations, baselines, conv_grid, conv_grid)
+        # kernel and out_data need to be same type -> this may need to be complex
+        # -> 1 at one space in the grid and zeros elsewhere (0, 0) or (conv_grid/2, conv_grid/2)
+
+        ## TODO: have the npol, nant be passed in as well to avoid magic numbers
+        illum = np.zeros(
+            shape=(1, 1, (self.nant*(self.nant-1)) // 2, 4, self.conv_grid, self.conv_grid),
+            dtype=np.complex64
+        )
+        illum[:, :, :, int(self.conv_grid/2), int(self.conv_grid/2)] = 1
+        gpu_illum = BFArray(illum, space="cuda")
+
+        uvw = np.zeros(
+            shape=(1, 1, 3, (self.nant*(self.nant-1)) // 2, 2),
+            dtype=np.int32
+        )
+        gpu_uvw = BFArray(uvw.transpose(2,0,1,3,4), space="cuda")
+
+        # out data in shape of (channels, polarisations, grid_size, grid_size)
+        self.log.info("Attempting to allocate %d bytes of GPU memory for gridder output" % self.ogulp_size)
+        out_data = BFArray(np.zeros(
+            shape=(1, self.npol, self.grid_size, self.grid_size),
+            dtype=np.complex64),
+            space="cuda",
+        )
+
+        romein_kernel = Romein()
+        romein_kernel.init(
+            gpu_uvw,
+            gpu_illum,
+            self.grid_size,
+            polmajor=False,
+        )
+
+        with self.oring.begin_writing() as oring:
+            for iseq in self.iring.read(guarantee=self.guarantee):
+                ihdr = json.loads(iseq.header.tostring())
+                nchan = ihdr['nchan']
+                ohdr = ihdr.copy()
+                # Mash header in here if you want
+                ohdr_str = json.dumps(ohdr)
+                prev_time = time.time()
+                chan_id = 0
+                self.log.info("Starting output sequence with nringlet %d" % iseq.nringlet)
+                #with oring.begin_sequence(time_tag=iseq.time_tag, header=ohdr_str, nringlet=iseq.nringlet) as oseq:
+                for ispan in iseq.read(self.igulp_size):
+                    idata = ispan.data_view("ci32").reshape(1, 1, (self.nant * (self.nant-1))//2, self.npol**2).transpose(0,1,3,2)
+                    self.log.info("Input buffer shape: %s" % str(idata.shape))
+                    self.log.info("Output buffer shape: %s" % str(out_data.shape))
+                    if ispan.size < self.igulp_size:
+                        continue # Ignore final gulp
+                    curr_time = time.time()
+                    acquire_time = curr_time - prev_time
+                    prev_time = curr_time
+                    romein_kernel.execute(idata, out_data)
+                    chan_id += 1
+                    if chan_id == nchan:
+                        chan_id = 0
+                        self.log("Gridding completed for %d channels" % nchan)
+                        #with oseq.reserve(self.ogulp_size) as ospan:
+                        #    copy_array(ospan.data, out_data)
+                        #if (self.oring.space == 'cuda') or (self.iring.space=='cuda'):
+                        #    stream_synchronize()
+
