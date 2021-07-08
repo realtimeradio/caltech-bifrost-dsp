aabfb34f6b20880a5bd165e42940d10762fee75f
ubuntu <ubuntu@pm.me>
2021-07-08 19:37:07 +0000

    Add more script options

--- pipeline-control/scripts/lwa352_arm_correlator.py
+++ pipeline-control/scripts/lwa352_arm_correlator.py
@@ -1,39 +1,58 @@
 import argparse
+import time
 from lwa352_pipeline_control import Lwa352PipelineControl
 
 def arm_and_wait(blocks, delay):
+    assert delay > 5, "I won't arm <5 seconds in the future."
+    corr_arm_time = blocks[0].get_next_allowed_start(delay)
+
     for b in blocks:
         b.set_start_time(corr_arm_time)
+    time.sleep(1)
     
+    ok = True
     for b in blocks:
         if b.get_bifrost_status(user_only=True)['state'] != 'waiting':
+            ok = False
             print("Pipeline %s:%d not in expected waiting state after arm" %
                     (b._host, b._pipeline_id))
 
-    wait_time = args.delay + 3
+    if ok:
+        print("All pipelines in 'waiting' state as expected")
+
+    wait_time = delay + 3
     print("Waiting %d seconds for trigger" % wait_time)
     time.sleep(wait_time) # Trigger should have happened by now
                       
+    ok = True
     for b in blocks:
         if b.get_bifrost_status(user_only=True)['state'] != 'running':
+            ok = False
             print("Pipeline %s:%d not in expected running state" %
                     (b._host, b._pipeline_id))
+
+    if ok:
+        print("All pipelines in 'running' state as expected")
 
 def main(args):
     controllers = []
     for host in args.hosts:
-        for p in args.pipelines:
+        for p in range(args.pipelines):
             controllers += [Lwa352PipelineControl(
                                host=host,
                                pipeline_id=p,
                                etcdhost=args.etcdhost)
                            ]
+
+    # First set up destination
+    for c in controllers:
+        c.corr_output_full.set_max_mbps(args.maxmbps)
+        c.corr_output_full.set_destination(dest_ip=args.destip, dest_port=args.destport)
+
     # First arm fast correlator
-    corr_arm_time = controllers[0].corr.get_next_allowed_start(args.delay)
     arm_and_wait([c.corr for c in controllers], args.delay)
     # Next arm accumulator
-    corr_arm_time = controllers[0].corr_acc.get_next_allowed_start(args.delay)
-    arm_and_wait([c.corracc for c in controllers], args.delay)
+    arm_and_wait([c.corr_acc for c in controllers], args.delay)
 
 
 if __name__ == "__main__":
@@ -43,10 +62,16 @@ if __name__ == "__main__":
         )
     parser.add_argument('-p', '--pipelines', type=int, default='2',
                         help='Number of pipelines per host')
-    parser.add_argument('hosts',
+    parser.add_argument('hosts', nargs='+',
                         help='Hostnames of servers running pipelines')
     parser.add_argument('-d', '--delay', type=int, default=10,
                         help='How long in future to arm correlator')
+    parser.add_argument('--destip', type=str, default='10.41.0.19',
+                        help='Destination IP for slow correlation data')
+    parser.add_argument('--destport', type=int, default=10001,
+                        help='Destination UDP port for slow correlation data')
+    parser.add_argument('--maxmbps', type=int, default=1500,
+                        help='Maximum Mbits/s output for each pipeline')
     parser.add_argument('--etcdhost', default='etcdv3service.sas.pvt',
                         help='etcd host to which stats should be published')
     args = parser.parse_args()
