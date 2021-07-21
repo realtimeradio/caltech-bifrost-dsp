0e3086927a1cfdcb67e385152929f58d458447c8
ubuntu <ubuntu@pm.me>
2021-07-21 19:19:51 +0000

    Update power beam output control

--- pipeline-control/lwa352_pipeline_control/blocks/beamform_output_control.py
+++ pipeline-control/lwa352_pipeline_control/blocks/beamform_output_control.py
@@ -1,14 +1,10 @@
 from .block_control_base import BlockControl
 
 class BeamformOutputControl(BlockControl):
-    def set_destination(self, dest_ip, dest_port):
-        assert isinstance(dest_ip, str)
-        assert isinstance(dest_port, int)
+    def set_destination(self, dest_ips, dest_ports):
+        assert isinstance(dest_ips, list)
+        assert isinstance(dest_ports, list)
         self._send_command(
-            dest_ip = dest_ip,
-            dest_port = dest_port,
+            dest_ip = dest_ips,
+            dest_port = dest_ports,
         )
-
-    def set_packet_delay(self, delay_ns):
-       assert isinstance(delay_ns, int)
-       self._send_command(packet_delay_ns=delay_ns)
