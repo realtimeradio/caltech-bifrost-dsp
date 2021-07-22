4a423c3f555ff853b06584c6e59b4c4e0ccbcce0
ubuntu <ubuntu@pm.me>
2021-07-22 17:00:39 +0000

    Make logging quieter

--- pipeline/lwa352_pipeline/blocks/corr_output_full_block.py
+++ pipeline/lwa352_pipeline/blocks/corr_output_full_block.py
@@ -420,7 +420,7 @@ class CorrOutputFull(Block):
         return np.frombuffer(dtest_raw, dtype=np.complex).reshape(dim)
 
     def send_packets_py(self, sync_time, this_gulp_time, bw_hz, sfreq,
-                              upstream_acc_len, chan0):
+                              upstream_acc_len, chan0, verbose=False):
         cpu_affinity.set_core(self.core)
         start_time = time.time()
         packet_cnt = 0
@@ -457,7 +457,8 @@ class CorrOutputFull(Block):
         stop_time = time.time()
         elapsed = stop_time - start_time
         gbps = 8*self.dump_size / elapsed / 1e9
-        self.log.info("CORR OUTPUT >> Sending complete for time %d in %.2f seconds (%f Gb/s)" % (this_gulp_time, elapsed, gbps))
+        if verbose:
+            self.log.info("CORR OUTPUT >> Sending complete for time %d in %.2f seconds (%f Gb/s)" % (this_gulp_time, elapsed, gbps))
         self.update_stats({'output_gbps': gbps})
 
     def print_autos(self):
@@ -474,7 +475,7 @@ class CorrOutputFull(Block):
             print("%d-X-ReorderedT:"%i, sdata2[0:NCHAN_PRINT,0,0])
             print("%d-Y-ReorderedT:"%i, sdata2[0:NCHAN_PRINT,1,1])
 
-    def send_packets_bf(self, udt, time_tag, desc, chan0, gain, navg, tuning):
+    def send_packets_bf(self, udt, time_tag, desc, chan0, gain, navg, tuning, verbose=False):
         cpu_affinity.set_core(self.core)
         start_time = time.time()
         desc.set_chan0(chan0)
@@ -512,7 +513,8 @@ class CorrOutputFull(Block):
         stop_time = time.time()
         elapsed = stop_time - start_time
         gbps = 8 * self.dump_size / elapsed / 1e9
-        self.log.info("CORR OUTPUT >> Sending complete for time_tag %d in %.2f seconds (%d Bytes; %.2f Gb/s)" % (time_tag, elapsed, self.dump_size, gbps))
+        if verbose:
+            self.log.info("CORR OUTPUT >> Sending complete for time_tag %d in %.2f seconds (%d Bytes; %.2f Gb/s)" % (time_tag, elapsed, self.dump_size, gbps))
         self.update_stats({'output_gbps': gbps})
 
     def check_against_file(self, upstream_acc_len, upstream_start_time):
@@ -587,6 +589,7 @@ class CorrOutputFull(Block):
             nchan = ihdr['nchan']
             chan0 = ihdr['chan0']
             bw_hz = ihdr['bw_hz']
+            print_on_send = True
             if self.use_cor_fmt:
                 samples_per_spectra = int(nchan * ihdr['fs_hz'] / bw_hz)
                 this_pipeline = (chan0 // nchan) % self.npipeline
@@ -602,6 +605,7 @@ class CorrOutputFull(Block):
                     continue # skip last gulp
                 # Update destinations if necessary
                 if self.update_pending:
+                    print_on_send = True
                     self.update_command_vals()
                     if self.command_vals['dest_file'] != "":
                         self.log.info("CORR OUTPUT >> Updating destination to file %s (max rate %.1f Mbits/s)" % 
@@ -643,11 +647,14 @@ class CorrOutputFull(Block):
                     if self.use_cor_fmt:
                         time_tag = this_gulp_time * samples_per_spectra
                         self.send_packets_bf(udt, time_tag, desc, chan0, 0, upstream_acc_len,
-                                (self.npipeline << 8) + (this_pipeline + 1))
+                                (self.npipeline << 8) + (this_pipeline + 1), verbose=print_on_send)
                     else:
-                        self.send_packets_py(ihdr['sync_time'], this_gulp_time, bw_hz, sfreq, upstream_acc_len, chan0)
+                        self.send_packets_py(ihdr['sync_time'], this_gulp_time, bw_hz, sfreq,
+                                upstream_acc_len, chan0, verbose=print_on_send)
                 else:
-                    self.log.info("CORR OUTPUT >> Skipping sending for time %d" % this_gulp_time)
+                    if print_on_send:
+                        self.log.info("CORR OUTPUT >> Skipping sending for time %d" % this_gulp_time)
+                print_on_send = False
                 curr_time = time.time()
                 process_time = curr_time - prev_time
                 prev_time = curr_time
