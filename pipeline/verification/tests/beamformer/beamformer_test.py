82fdbe7179ae6d5a0c71fe9d8cf6099ad0e14ba7
ubuntu <ubuntu@pm.me>
2021-08-24 14:52:43 +0000

    Make beamformer test bench

--- pipeline/verification/tests/beamformer/beamformer_test.py
+++ pipeline/verification/tests/beamformer/beamformer_test.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import sys
+import os
 import time
 import logging
 import threading
@@ -15,10 +16,11 @@ from lwa352_pipeline.blocks.copy_block import Copy
 from lwa352_pipeline.blocks.beamform_block import Beamform 
 from lwa352_pipeline.blocks.block_base import Block
 
-NSTAND = 352
+NSTAND = 40 #352
 NPOL = 2
-NTIME_GULP = 480
-NCHAN = 96
+NTIME_GULP = 120 # 480
+NGULP = 5 # Number of gulps of random data to generate
+NCHAN = 16 # 96
 GPUDEV = 0
 ETCD_HOST = "etcdv3service.sas.pvt"
 TESTFILE_NAME = '/tmp/temp_testfile.dat'
@@ -26,13 +28,46 @@ NBEAM = 16
 
 CORES = list(range(12))
 
-class BeamPrinter(Block):
+class SoftwareBf(Block):
     def __init__(self, log, iring, ntime_gulp=2500,
-                 guarantee=True, core=-1):
+            guarantee=True, core=-1, testfile=None, coeffs=None):
 
-        super(BeamPrinter, self).__init__(log, iring, None, guarantee, core, etcd_client=None)
+        super(SoftwareBf, self).__init__(log, iring, None, guarantee, core, etcd_client=None)
         cpu_affinity.set_core(self.core)
         self.ntime_gulp = ntime_gulp
+        # file containing test data
+        if testfile is not None:
+            self.testfile = open(testfile, 'rb')
+            self.testfile_nbytes = os.path.getsize(testfile)
+        else:
+            self.testfile = None
+
+        self.coeffs = coeffs
+        self.nchan, self.nbeam, self.ninput = self.coeffs.shape
+
+    def get_input_gulp(self, t):
+        nbytes = self.ntime_gulp * self.nchan * self.ninput
+        seekloc = (t*nbytes) % self.testfile_nbytes
+        self.testfile.seek(seekloc)
+        rawdata = self.testfile.read(nbytes)
+        uint_data = np.frombuffer(rawdata, dtype=np.uint8).reshape([self.ntime_gulp, self.nchan, self.ninput])
+        complex_data = np.zeros_like(uint_data, dtype=np.complex64)
+        r = np.array((uint_data >> 4) & 0xf, dtype=np.int8)
+        r[r>7] -= 16
+        i = np.array(uint_data & 0xf, dtype=np.int8)
+        i[i>7] -= 16
+        complex_data = r + 1j*i
+        return complex_data
+
+    def cpu_beamform(self, data):
+        # data: [time x chan x input]
+        # coeffs: [chan x beam x input]
+        out = np.zeros([self.nchan, self.nbeam, self.ntime_gulp], dtype=np.complex64)
+        for t in range(self.ntime_gulp):
+            for c in range(self.nchan):
+                for b in range(self.nbeam):
+                    out[c,b,t] = np.sum(self.coeffs[c,b,:] * data[t,c,:])
+        return out
 
     def main(self):
         cpu_affinity.set_core(self.core)
@@ -40,30 +75,47 @@ class BeamPrinter(Block):
         tick = time.time()
         for iseq in self.iring.read(guarantee=self.guarantee):
             gulpi = 0
-            ihdr = json.loads(iseq.header.tostring())
+            ihdr = json.loads(iseq.header.tobytes())
             nstand = ihdr['nstand']
             npol = ihdr['npol']
             nbeam = ihdr['nbeam']
             nchan = ihdr['nchan']
-            #print(ihdr)
+            seq = ihdr['seq0']
             igulp_size = self.ntime_gulp * nchan * nbeam * 2 * 4 # complex float32
+            running_max = 0
             for ispan in iseq.read(igulp_size):
                 idata = ispan.data_view('cf32').reshape([nchan, nbeam, self.ntime_gulp])
-                if time.time() - tick > 1:
-                    print('Gulp: %d' % gulpi)
-                    print('Time (beam0, chan0):', idata[0, 0, 0:10])
-                    print('Chan (time 0, beam 0):', idata[:,0,0])
-                    print('Beam (time 0, chan0):', idata[0,:,0])
-                    tick = time.time()
+                test_data = self.get_input_gulp(seq // self.ntime_gulp)
+                cpudata = self.cpu_beamform(test_data)
+                #print(idata[0,0,0:10])
+                #print(cpudata[0,0,0:10])
+                maxdiff = np.max(np.abs(idata - cpudata)/np.abs(idata))
+                if maxdiff > running_max:
+                    running_max = maxdiff
+                assert np.all(np.isclose(idata, cpudata, atol=1e-4)), "CPU/GPU match fail! Max frac diff %f" % maxdiff
+                seq += self.ntime_gulp
+                print("MATCH OK after %d times (max frac diff %f)" % (seq, running_max))
+                #if time.time() - tick > 1:
+                #    print('Gulp: %d' % gulpi)
+                #    print('Time (beam0, chan0):', idata[0, 0, 0:10])
+                #    print('Chan (time 0, beam 0):', idata[:,0,0])
+                #    print('Beam (time 0, chan0):', idata[0,:,0])
+                #    tick = time.time()
                 gulpi += 1
 
 def main():
-    test_data = np.ones([NTIME_GULP, NCHAN, NSTAND, NPOL], dtype=np.uint8)
-    #for s in range(NSTAND):
-    #    test_data[:, :, s, :] = (NSTAND % 8) << 4
+    rng = np.random.default_rng(0xaabbccdd)
+    print("Generating test data")
+    test_data = rng.integers(0, high=255, size=[NGULP*NTIME_GULP, NCHAN, NSTAND, NPOL], dtype=np.uint8)
+
+    print("Writing test data to file")
     with open(TESTFILE_NAME, 'wb') as fh:
         print("Writing testfile: %s" % TESTFILE_NAME)
         fh.write(test_data.tobytes())
+
+    print("Generating random coefficients")
+    coeffs = 3*(rng.random(size=[NCHAN, 2*NBEAM, NSTAND*NPOL])-0.5) + 4*1j*(rng.random(size=[NCHAN, 2*NBEAM, NSTAND*NPOL])-0.5)
+    coeffs = np.array(coeffs, dtype=np.complex64)
     
     etcd_client = etcd.client(ETCD_HOST)
 
@@ -94,11 +146,14 @@ def main():
                       core=CORES.pop(0), guarantee=True, gpu=GPUDEV, ntime_sum=None,
                       etcd_client=etcd_client))
 
+    ops[-1].gains_cpu[...] = coeffs
+
     ops.append(Copy(log, iring=bf_output_ring, oring=output_ring, ntime_gulp=NTIME_GULP,
                       nbyte_per_time=2*NBEAM*NCHAN*2*4,
                       core=CORES.pop(0), guarantee=True, gpu=GPUDEV))
 
-    ops.append(BeamPrinter(log, iring=output_ring, ntime_gulp=NTIME_GULP))
+    ops.append(SoftwareBf(log, iring=output_ring, ntime_gulp=NTIME_GULP,
+        testfile=TESTFILE_NAME, coeffs=coeffs))
 
     threads = [threading.Thread(target=op.main) for op in ops]
     
