8a1df0d574f2253c0d489dbd8b24ed0480e75491
Nikita Kosogorov <nikita@Nikitas-MacBook-Pro.local>
2023-06-19 14:06:56 -0700

    compute weights for offline beamforming

--- pipeline/lwa352_pipeline/blocks/beamform_offline_block.py
+++ pipeline/lwa352_pipeline/blocks/beamform_offline_block.py
@@ -2,33 +2,137 @@ from copy import deepcopy
 import numpy as np
 from bifrost.pipeline import TransformBlock
 from bifrost.linalg import LinAlg
+from astropy.constants import c as speedOfLight
+speedOfLight = speedOfLight.to('m/ns').value
+
+from lwa_antpos.station import ovro
 from bifrost import ndarray as BFArray
+
+from astropy import units as u
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from astropy.time import Time
+from astropy.coordinates import Angle
+
 
 linalg = LinAlg()
 
+NPOL = 2
 class BfOfflineBlock(TransformBlock):
-    def __init__(self, iring, nbeam, ntimestep, ant_locs, *args, **kwargs):
+    def __init__(self, iring, nbeam, ntimestep, ra_array, dec_array, station=ovro,frame_size=None, *args, **kwargs):
         super(BfOfflineBlock, self).__init__(iring, *args, **kwargs)
         self.nbeam = nbeam # Number of beams to form
         self.ntimestep = ntimestep # Number of time samples between coefficient updates
-        self.ant_locs = ant_locs # Antenna locations
+        self.station = station # station
+        self.frame_size = frame_size  # Frame size
+        self.ra_array = ra_array  # Right Ascension array
+        self.dec_array = dec_array  # Declination array
 
-    def compute_coefficients(self, time):
-        # Take time, antenna locations; update coefficients
-        # self.coeffs = <....>
-        pass
+    def set_beam_target(self, ra, dec, observation_time, verbose=True):
+        """
+        Given a target's RA and DEC (degrees), and the time of observation, compute the
+        topocentric position of the target and point the beam at it.
+        """
+
+        # Load in where we are
+        obs = EarthLocation.from_geocentric(*self.station.ecef, unit=u.m)
+
+        # Convert RA and DEC to SkyCoord object
+        ra = Angle(ra, unit=u.deg)
+        dec = Angle(dec, unit=u.deg)
+        sc = SkyCoord(ra, dec, frame='icrs')
+
+        # Figure out where it is at the given observation time
+        compute_time = Time(observation_time, format='unix', scale='utc')
+        aa = sc.transform_to(AltAz(obstime=compute_time, location=obs))
+        az = aa.az.to(u.rad).value  # Convert to radians
+        alt = aa.alt.to(u.rad).value  # Convert to radians
+        if verbose:
+            print(f"At observation time, target is at azimuth {aa.az}, altitude {aa.alt}")
+
+        # Return azimuth and altitude in radians
+        return az, alt
+
+    def set_beam_weighting(self, fnc=lambda x: 1.0):
+        """
+        Set the beamformer antenna weighting using the provided function.  The
+        function should accept a single floating point input of an antenna's
+        distance from the array center (in meters) and return a weight between
+        0 and 1, inclusive.
+        """
+        fnc2 = lambda x: np.clip(fnc(np.sqrt(x[0] ** 2 + x[1] ** 2)), 0, 1)
+        self._weighting = np.array([fnc2(ant.enz) for ant in self.station.antennas])
+        self._weighting = np.repeat(self._weighting, 2)
+
+    def compute_weights(self, observation_time):
+        assert len(self.ra_array) == self.nbeam, "Mismatch in number of ra angles and beams"
+        assert len(self.dec_array) == self.nbeam, "Mismatch in number of dec angles and beams"
+
+        weights_array = []
+        for beam in range(self.nbeam):
+            ra = self.ra_array[beam]
+            dec = self.dec_array[beam]
+
+            # Set beam target and get az and alt values
+            az, alt = self.set_beam_target(ra, dec, observation_time)
+
+            assert (az >= 0 and az < 2 * np.pi)
+            assert (alt >= 0 and alt <= np.pi / 2)
+
+            zenith = np.array([0, 0, 1])
+            zenith_delay = [np.dot(zenith, antenna.enz) / speedOfLight for antenna in self.station.antennas]
+
+            direction = np.array([np.cos(alt) * np.sin(az), np.cos(alt) * np.cos(az), np.sin(alt)])
+            direction_delay = [np.dot(direction, antenna.enz) / speedOfLight for antenna in self.station.antennas]
+
+            delays = np.array(direction_delay) - np.array(zenith_delay)
+            delays = np.repeat(delays, NPOL)
+            delays = delays.max() - delays
+
+            # Compute antenna weights
+            antenna_weights = np.repeat(self._weighting, NPOL)
+
+            # Multiply delays by antenna weights
+            weighted_delays = antenna_weights * delays
+
+            weights = np.exp(2j * np.pi * self.frequencies[:, None] * weighted_delays * 1e-9)
+            weights_array.append(weights)
+
+
+        return np.array(weights_array)  # Convert list of weights into an array
 
     def on_sequence(self, iseq):
         """
         At the start of a sequence, figure out how many stands / pols / chans
         we are dealing with, and construct an array for coefficients.
         """
+        # Get the observation time from the tensor header
+        self.tstart_unix, self.tstep_s = iseq.header['_tensor']['scales'][0]
+        self.gulp_nframe = iseq.header['_tensor']['gulp_nframe']
+
+        sfreq, fstep_hz = iseq.header['_tensor']['scales'][2]
+        self.frequencies = sfreq + np.arange(iseq.header['nchan']) * fstep_hz
+
+
         ohdr = deepcopy(iseq.header)
+
         # Create empty coefficient array
-        self.coeffs = BfArray(np.zeros([hdr['nchan'], hdr['nstand'], hdr['npol']], dtype=complex), space='cuda_host')
+        self.coeffs = BFArray(
+            np.zeros([self.nbeam, iseq.header['nchan'], iseq.header['nstand'], iseq.header['npol']], dtype=complex),
+            space='cuda_host')
         # Manipulate header. Dimensions will be different (beams, not stands)
-        #hdr['_tensor'] = {
-        #    }
+        ohdr['_tensor'] = {
+            'dtype': 'ci' + str(ohdr['nbit']),
+            'shape': [-1, self.frame_size, ohdr['nchan'], self.nbeam, ohdr['npol']],
+            'labels': ['time', 'fine_time', 'freq', 'beam', 'pol'],
+            'scales': [(self.tstart_unix, self.tstep_s * self.frame_size),
+                       (0, self.tstep_s),
+                       (ohdr['sfreq'], fstep_hz),
+                       None,
+                       None],
+            'units': ['s', 's', 'Hz', None, None],
+            'gulp_nframe': self.gulp_nframe,
+        }
+
         self.nframe_read = 0
         return ohdr
 
@@ -39,9 +143,26 @@ class BfOfflineBlock(TransformBlock):
         idata = ispan.data
         odata = ospan.data
 
-        linalg(1, idata, self.coeffs, 0, odata) # These arguments aren't right, but there is a matrix multiply here
-        self.nframe_read += in_nframe
-        some_time_s
-        self.compute_coefficients(self.nframe_read * some_time_scale) # FIXME
+        # Calculate the observation time for the current gulp
+        gulp_start_time = self.tstart_unix + self.tstep_s * ispan.sequence
+
+        # Update the coefficients every ntimestep
+        for i in range(in_nframe):
+            if self.nframe_read % self.ntimestep == 0:
+                # Compute the observation time for the current frame
+                observation_time = gulp_start_time + self.tstep_s * i / self.gulp_nframe
+                self.coeffs = self.compute_weights(observation_time)
+
+                # Add an extra dimension for "fine_time" before transposing
+                self.coeffs = np.expand_dims(self.coeffs, axis=1)
+
+                # Transpose coefficients to match the dimensions for multiplication
+                self.coeffs = np.transpose(self.coeffs, (2, 1, 0, 3, 4))  # [nstand, fine_time, nchan, nbeam, npol]
+
+
+            # Perform beamforming operation for the current frame
+            linalg(1, idata[i], self.coeffs, 0, odata[i])
+
+            self.nframe_read += 1
 
         return out_nframe
