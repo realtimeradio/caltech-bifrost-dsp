c2468a2d9c470d0761d1d2a7cf89be3580a04505
Nikita Kosogorov <nikita@Nikitas-MacBook-Pro.local>
2023-07-20 21:56:17 -0700

    Added capability to process beams in batches

--- pipeline/lwa352_pipeline/blocks/beamform_offline_block.py
+++ pipeline/lwa352_pipeline/blocks/beamform_offline_block.py
@@ -18,14 +18,21 @@ linalg = LinAlg()
 
 NPOL = 2
 class BfOfflineBlock(TransformBlock):
-    def __init__(self, iring, nbeam, ntimestep, ra_array, dec_array, station=ovro,frame_size=1, *args, **kwargs):
+    def __init__(self, iring, nbeam, nbeams_per_batch, ntimestep, ra_array, dec_array, station=ovro,frame_size=1, *args, **kwargs):
         super(BfOfflineBlock, self).__init__(iring, *args, **kwargs)
         self.nbeam = nbeam # Number of beams to form
+        self.nbeams_per_batch = nbeams_per_batch
+        if nbeam % nbeams_per_batch != 0:
+            raise ValueError(f"Total number of beams ({nbeam}) must be divisible by number of beams per batch ({nbeams_per_batch})")
+
         self.ntimestep = ntimestep # Number of time samples between coefficient updates
         self.station = station # station
         self.frame_size = frame_size  # Frame size
         self.ra_array = ra_array  # Right Ascension array
         self.dec_array = dec_array  # Declination array
+
+        assert len(self.ra_array) == self.nbeam, "Mismatch in number of ra angles and beams"
+        assert len(self.dec_array) == self.nbeam, "Mismatch in number of dec angles and beams"
 
         # Initially set uniform antenna weighting for a natural beam shape
         self.set_beam_weighting(lambda x: 1.0)
@@ -66,14 +73,15 @@ class BfOfflineBlock(TransformBlock):
         self._weighting = np.array([fnc2(ant.enz) for ant in self.station.antennas])
         self._weighting = np.repeat(self._weighting, 2)
 
-    def compute_weights(self, observation_time):
-        assert len(self.ra_array) == self.nbeam, "Mismatch in number of ra angles and beams"
-        assert len(self.dec_array) == self.nbeam, "Mismatch in number of dec angles and beams"
+    def compute_weights(self, observation_time, batch_start, batch_end):
+
+        batch_ra_array = self.ra_array[batch_start:batch_end]
+        batch_dec_array = self.dec_array[batch_start:batch_end]
 
         weights_array = []
-        for beam in range(self.nbeam):
-            ra = self.ra_array[beam]
-            dec = self.dec_array[beam]
+        for beam in range(self.nbeams_per_batch):
+            ra = batch_ra_array[beam]
+            dec = batch_dec_array[beam]
 
             # Set beam target and get az and alt values
             az, alt = self.set_beam_target(ra, dec, observation_time)
@@ -117,7 +125,7 @@ class BfOfflineBlock(TransformBlock):
 
         # Create empty coefficient array
         self.coeffs = BFArray(
-            np.zeros([self.nbeam, iseq.header['nchan'], iseq.header['nstand'], iseq.header['npol']], dtype=complex),
+            np.zeros([self.nbeams_per_batch, iseq.header['nchan'], iseq.header['nstand'], iseq.header['npol']], dtype=complex),
             space='cuda_host')
         # Manipulate header. Dimensions will be different (beams, not stands)
         ohdr['_tensor'] = {
@@ -146,25 +154,27 @@ class BfOfflineBlock(TransformBlock):
         # Calculate the observation time for the current gulp
         gulp_start_time = self.tstart_unix + self.tstep_s * ispan.sequence
 
-        # Update the coefficients every ntimestep
-        for i in range(in_nframe):
-            if self.nframe_read % self.ntimestep == 0:
-                # Compute the observation time for the current frame
-                observation_time = gulp_start_time + self.tstep_s * i / self.gulp_nframe
-                self.coeffs = self.compute_weights(observation_time)
+        for batch_start in range(0, self.nbeam, self.nbeams_per_batch):
+            batch_end = min(batch_start + self.nbeams_per_batch, self.nbeam)
 
-                # Reshape to (nbeam, nchan, nstand, npol)
-                self.coeffs = self.coeffs.reshape(self.nbeam, self.nchan, self.nstand, self.npol)
-                # Swap nchan and nbeam
-                self.coeffs = self.coeffs.transpose(1, 0, 2, 3)
+            for i in range(in_nframe):
+                if self.nframe_read % self.ntimestep == 0:
+                    observation_time = gulp_start_time + self.tstep_s * i / self.gulp_nframe
+                    batch_coefficients = self.compute_weights(observation_time, batch_start, batch_end)
+                    # Reshape to (nbeams_per_batch, nchan, nstand, npol)
+                    batch_coefficients = batch_coefficients.reshape(self.nbeams_per_batch, self.nchan, self.nstand,
+                                                                    self.npol)
+                    # Swap nchan and nbeams_per_batch
+                    batch_coefficients = batch_coefficients.transpose(1, 0, 2, 3)
+                    # Update the coefficients in self.coeffs for the current batch
+                    self.coeffs[:] = batch_coefficients
 
-            for j in range(self.frame_size):
-                # Perform element-wise multiplication and then sum over the nstand axis
-                odata[i, j] = np.sum(self.coeffs * idata[i, j][:, np.newaxis, :, :], axis=2)
+                for j in range(self.frame_size):
+                    # Perform element-wise multiplication and then sum over the nstand axis?
+                    odata[i, j] = np.sum(self.coeffs * idata[i, j][:, np.newaxis, :, :], axis=2)
+
+                self.nframe_read += 1
 
 
-            ##linalg(1, idata[i], self.coeffs, 0, odata[i])
-
-            self.nframe_read += 1
 
         return out_nframe
